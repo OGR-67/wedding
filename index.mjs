6fecd7d7f988b123d7803f987e0e3af3218eb4a0
Olivier Roos <roosolivier@gmail.com>
2022-07-10 16:12:47 +0200

    added a Procfile

--- index.mjs
+++ index.mjs
@@ -11,7 +11,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://localhost/ExpressDB", {
   useNewUrlParser: true,
